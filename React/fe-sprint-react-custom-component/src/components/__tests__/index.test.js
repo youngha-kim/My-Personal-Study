87373317afc83c2b13820a5422eaa9cc8a274202
younha-kim <rladudtmd727@gmail.com>
2022-07-05 03:12:09 +0900

    add AutoCompleteFunction in react-custom-component

--- React/fe-sprint-react-custom-component/src/components/__tests__/index.test.js
+++ React/fe-sprint-react-custom-component/src/components/__tests__/index.test.js
@@ -12,14 +12,14 @@ import '@testing-library/jest-dom';
 //   require('./Tab.test');
 // });
   
-describe('Tag UI 컴포넌트 기능 테스트', () => {
-  require('./Tag.test');
-});
+// describe('Tag UI 컴포넌트 기능 테스트', () => {
+//   require('./Tag.test');
+// });
 
 // ! advanced challenge를 위해 주석을 해제하세요.  
-// describe('Autocomplete UI 컴포넌트 기능 테스트', () => {
-//   require('./Autocomplete.test');
-// });
+describe('Autocomplete UI 컴포넌트 기능 테스트', () => {
+  require('./Autocomplete.test');
+});
 
 // describe('ClickToEdit UI 컴포넌트 기능 테스트', () => {
 //   require('./ClickToEdit.test');
