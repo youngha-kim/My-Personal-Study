d404c0311ac577d6d34866a866b501f9c74c8a77
younha-kim <rladudtmd727@gmail.com>
2022-10-21 21:11:32 +0900

    learn typeScript -genric2

--- TypeScript/ts5_ploymorphism.ts
+++ TypeScript/ts5_ploymorphism.ts
@@ -16,8 +16,12 @@ console.log(superPrint(["a", "b", "c"]));
 
 // concrete tpye이란, 우리가 전부터 봐왔던 타입을 말한다.
 // number , boolean , void , 등등
+
+
 // generic이란 타입의 placeholder같은 것이고, concrete type를 대신하여 쓸 수 있다.
 // 타입 스크립트는 generic타입을 타입추론을 통해 사용하여 다음 들어올 확실한 타입을 알 수 없을 때 사용한다.
+// 즉, 제네릭은 선언 시점이 아니라 생성 시점에 타입을 명시하여 하나의 타입만이 아닌 다양한 타입을 사용할 수 있도록 하는 기법이다.
+
 // console.log(superPrint([1, 2, true]))
 
 // 이 때 call signituare을 사용하여
@@ -32,10 +36,10 @@ const superPrint1: SuperPrint1 = (arr) => arr[0];
 
 console.log("==========")
 console.log(superPrint1([1, 2, "string", true]));
-const a = superPrint1([4]);
-console.log(a);
+const a1= superPrint1([4]);
+console.log(a1);
 
-console.log(a + 4)
+console.log(a1 + 4)
 // 에러가 출력되지 않음을 알 수 있다.
 
 
@@ -46,3 +50,50 @@ console.log(a + 4)
 
 //=============================
 // generic in use raect 
+
+
+// 함수형으로 구현하기 
+function superPrint2<T>(a: T[]){
+  return a[0]
+}
+
+const a2 = superPrint2([1,2,3,4])
+
+//==========
+// 제네릭을 이용하면, 원하는 대로 코드를 확장하는 것이 가능해진다.
+// 타입을 생성하고 그 타입을 또다른 타입에 넣어서 사용할 수 있다.
+type Player<E> = {
+   name : string,
+   extraInfo : E
+}
+type YounghaExtra = {
+  favFood: string
+}
+
+type YounghaPlay = Player<YounghaExtra>
+
+const youngha : YounghaPlay= {
+  name : "youngha",
+  extraInfo:{
+    favFood : "김치"
+  }
+}
+
+// 만약 많은 것들이 있는 큰 타입 하나 가지고 있는데, 그 중 하나가 달라질 수 있는 타입이라면, 
+// 거기에 제네릭을 넣으면 된다.
+// 그러면 타입을 다양하게 재사용할 수 있다. 
+
+const lynn : Player<null> = {
+  name : "lynn",
+  extraInfo: null
+}
+
+
+/// 기본적인 타입스크립트의 타입은 제네릭으로 만들어져 있다 .
+type A = Array<number>
+
+let a:A = [ 1, 2, 3, 4]
+
+
+function printAllNumbers(arr: Array<number>){}
+
