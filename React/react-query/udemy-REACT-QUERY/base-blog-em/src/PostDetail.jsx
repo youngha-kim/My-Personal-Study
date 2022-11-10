c5c8d7f92c259ecb9c5c7ca551749765da809dbe
younha-kim <rladudtmd727@gmail.com>
2022-11-10 21:12:02 +0900

    learn react - query

---
+++ React/react-query/udemy-REACT-QUERY/base-blog-em/src/PostDetail.jsx
@@ -0,0 +1,55 @@
+import { useQuery } from "react-query";
+
+async function fetchComments(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+  return response.json();
+}
+
+async function deletePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+  return response.json();
+}
+
+async function updatePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+  );
+  return response.json();
+}
+
+export function PostDetail({ post }) {
+  // replace with useQuery
+  console.log(fetchComments(post.id));
+  const { data, isError, error, isLoading } = useQuery(["comments", post.id], () =>
+    fetchComments(post.id)
+  );
+  if (isLoading) return <h3>isloading...</h3>; // 데이터의 비동기 처리로 인해 undefined가 나올 수 있음.
+  if (isError)
+    return (
+      <>
+        <h3>Ops, someting went wrongs</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  console.log(data);
+
+  return (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button>Delete</button> <button>Update title</button>
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
---
+++ React/react-query/udemy-REACT-QUERY/base-blog-em/src/Posts.jsx
@@ -0,0 +1,83 @@
+import { current } from "@reduxjs/toolkit";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import { PostDetail } from "./PostDetail";
+
+const maxPostPage = 10;
+
+async function fetchPosts(pageNum) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
+  // replace with useQuery
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    { staleTime: 2000,
+      keepPreviousData : true, }
+  );
+  if (isLoading) return <h3>isloading...</h3>; // 데이터의 비동기 처리로 인해 undefined가 나올 수 있음.
+  if (isError)
+    return (
+      <>
+        <h3>Ops, someting went wrongs</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
+          Previous page
+        </button>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
