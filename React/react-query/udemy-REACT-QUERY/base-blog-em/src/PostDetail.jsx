1d07591ca8664c5a4f33d1aa51c077ee708b7d57
younha-kim <rladudtmd727@gmail.com>
2022-11-14 22:55:13 +0900

    learn react-query pagenation

--- React/react-query/udemy-REACT-QUERY/base-blog-em/src/PostDetail.jsx
+++ React/react-query/udemy-REACT-QUERY/base-blog-em/src/PostDetail.jsx
@@ -1,5 +1,5 @@
-import { useQuery } from "react-query";
-import {useMutation } from "react-query";
+import {  useQuery } from "react-query";
+import { useMutation } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -27,13 +27,17 @@ async function updatePost(postId) {
 export function PostDetail({ post }) {
   // replace with useQuery
   console.log(fetchComments(post.id));
-  const { data, isError, error, isLoading } = useQuery(["comments", post.id], () =>
-    fetchComments(post.id)
+  const { data, isError, error, isLoading } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id)
   );
 
-
-const deleteMutation = useMutation((postId)=>{deletePost(postId)})
-
+  const deleteMutation = useMutation((postId) => {
+    deletePost(postId);
+  });
+  const updateMutation = useMutation((postid) => {
+    updatePost(postid);
+  });
 
   if (isLoading) return <h3>isloading...</h3>; // 데이터의 비동기 처리로 인해 undefined가 나올 수 있음.
   if (isError)
@@ -47,17 +51,21 @@ const deleteMutation = useMutation((postId)=>{deletePost(postId)})
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button onClick={()=> deleteMutation.mutate(post.id)}>Delete</button> <button>Update title</button>
+     
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      <button onClick={()=> updateMutation.mutate(post.id)}>Update title</button>
       {deleteMutation.isError && (
-        <p style = {{color : "purple"}}>Error deleting the post</p>
+        <p style={{ color: "purple" }}>Error deleting the post</p>
       )}
       {deleteMutation.isLoading && (
-        <p style = {{color : "red"}}>deleting the post</p>
+        <p style={{ color: "red" }}>deleting the post</p>
       )}
       {deleteMutation.isSuccess && (
-        <p style = {{color : "green"}}>Post has (not) been deleted</p>
-      )}      
+        <p style={{ color: "green" }}>Post has (not) been deleted</p>
+      )}
+      {updateMutation.isError && <h3 style={{ color: "red" }}>{post.title}</h3>}
+     {updateMutation.isLoading && <h3 style={{ color: "black" }}>{post.title}</h3>}
+     {updateMutation.isSuccess && <h3 style={{ color: "blue" }}>{post.title}</h3>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
