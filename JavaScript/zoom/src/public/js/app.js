ad14752ac6421a9d9a8ee909217b44830d0838bb
younha-kim <rladudtmd727@gmail.com>
2022-12-01 21:07:07 +0900

    learn webRTC

--- JavaScript/zoom/src/public/js/app.js
+++ JavaScript/zoom/src/public/js/app.js
@@ -2,16 +2,20 @@
 // 웹에서 다운 받고,
 // io function 은 자동적으로 socket.io를 실행하고 있는 서버를 찾음.
 
-const socket = io("http://localhost:3000");
+const socket = io();
 
 const myface = document.getElementById("myFace");
 // stream은 비디오와 오디오가 결합된 것
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -22,7 +26,7 @@ async function getCameras() {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
-      if(currentCamera.label === camera.label){
+      if (currentCamera.label === camera.label) {
         option.selected = true;
       }
       camerasSelect.appendChild(option);
@@ -39,23 +43,24 @@ async function getMedia(deviceId) {
   };
   const cameraConstaraints = {
     audio: true,
-    video : {deviceId: {exact: deviceId}}
+    video: { deviceId: { exact: deviceId } },
   };
   try {
     myStream = await navigator.mediaDevices.getUserMedia(
       deviceId ? cameraConstaraints : initialConstraints
     );
     myface.srcObject = myStream;
-    if(!deviceId){
+    if (!deviceId) {
       await getCameras();
     }
-    
   } catch (e) {
     alert(e);
   }
 }
 
-getMedia();
+// getMedia();
+
+call.hidden = true;
 
 function handleMuteClick() {
   myStream
@@ -89,3 +94,47 @@ async function handleCameraChange() {
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+//welcome form (join a room )
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, startMedia);
+  roomName = input.value;
+  input.value = "";
+}
+
+async function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// socket code
+// peer A에서 실행된다.
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+});
+
+// peer B에서 실행된다.
+socket.on("offer", (offer) => {
+  console.log(offer);
+});
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
--- JavaScript/zoom/src/server.js
+++ JavaScript/zoom/src/server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import path from "path";
 import http from "http";
-import {Server} from "socket.io";
-
+import { Server } from "socket.io";
 
 // const path  = require("path")
 // const express = require("express")
 const app = express();
-const __dirname = path.join(path.resolve() , "/src" );
+const __dirname = path.join(path.resolve(), "/src");
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -16,13 +15,20 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-// socket io는 websocket의 부가기능이 아니다. 
-// 때문에 socketio를 브라우저에 설치해야 함. 
+// socket io는 websocket의 부가기능이 아니다.
+// 때문에 socketio를 브라우저에 설치해야 함.
 const io = new Server(httpServer);
 
-io.on("connection", socket => {
-  console.log(socket);
-})
+io.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+});
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
