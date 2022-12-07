13abc76f0bafd81dd18e4e6ea36cc0d368064973
younha-kim <rladudtmd727@gmail.com>
2022-12-07 20:03:37 +0900

    learn websocket

--- JavaScript/websocket/src/public/js/app.js
+++ JavaScript/websocket/src/public/js/app.js
@@ -1,1 +1,28 @@
-helllo;
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("form")
+// 여기의 socket은 서버로의 연결을 뜻한다.
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("connected to server ");
+})
+
+socket.addEventListener("message", (message) => {
+  console.log("new messages :  `", message.data, "` from the sever")
+})
+
+socket.addEventListener("close", () => {
+  console.log("disconnected to server ");
+})
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit)
+
+
+
--- JavaScript/websocket/src/server.js
+++ JavaScript/websocket/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
+import WebSocket from "ws";
 import path from "path";
-// import http from "http";
+import http from "http";
 // import { Server } from "socket.io";
 
 const app = express();
@@ -16,4 +17,27 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+//express를 기반으로 서버를 만든다.(필수아님)
+//서버를 보이게 노출시키고 그다음 http서버 위에 ws서버를 만들기 위함.
+const server = http.createServer(app);
+
+// 이제 http 서버를 기반으로 ws서버를 만든다.
+const wss = new WebSocket.Server({ server });
+
+// 여기서 socket은 연결된 브라우저를 뜻한다.
+// on method는 backend에 연결된 사람의 정보를 제공해준다.
+wss.on("connection", (socket) => {
+  console.log("connected to Browser ");
+  // 연결이 완료되면 프론트에 메세지를 보냄
+  socket.on("close", () => console.log("disconnected from the browser"));
+  socket.send("hello");
+  socket.on("message", (message) => {
+    const buf1 = Buffer.from([...message]);
+    socket.send(buf1.toString());
+  });
+});
+
+// 이제 http ,ws 서버는 다 같은 port를 공유하게된다.
+server.listen(3000, handleListen);
+// app.listen(3000, handleListen);
