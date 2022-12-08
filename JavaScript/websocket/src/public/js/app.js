6c07978b476b1cd30a8a504f7e5162ac7ca4b0df
younha-kim <rladudtmd727@gmail.com>
2022-12-08 23:05:52 +0900

    learn websocket

--- JavaScript/websocket/src/public/js/app.js
+++ JavaScript/websocket/src/public/js/app.js
@@ -1,28 +1,42 @@
-const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nickname");
+
 // 여기의 socket은 서버로의 연결을 뜻한다.
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
   console.log("connected to server ");
-})
+});
 
 socket.addEventListener("message", (message) => {
-  console.log("new messages :  `", message.data, "` from the sever")
-})
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
 
 socket.addEventListener("close", () => {
   console.log("disconnected to server ");
-})
+});
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
-}
+};
 
-messageForm.addEventListener("submit", handleSubmit)
+const hanleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+};
 
-
-
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", hanleNickSubmit);
--- JavaScript/websocket/src/server.js
+++ JavaScript/websocket/src/server.js
@@ -25,17 +25,35 @@ const server = http.createServer(app);
 // 이제 http 서버를 기반으로 ws서버를 만든다.
 const wss = new WebSocket.Server({ server });
 
+// 메시지를 다른 모든 socket에 전달할 수 있다.
+const sockets = [];
+
 // 여기서 socket은 연결된 브라우저를 뜻한다.
 // on method는 backend에 연결된 사람의 정보를 제공해준다.
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
   console.log("connected to Browser ");
   // 연결이 완료되면 프론트에 메세지를 보냄
   socket.on("close", () => console.log("disconnected from the browser"));
   socket.send("hello");
   socket.on("message", (message) => {
     const buf1 = Buffer.from([...message]);
-    socket.send(buf1.toString());
+    const parsed = JSON.parse(buf1.toString());
+
+    switch (parsed.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${parsed.payload}`)
+        );
+        break;
+      case "nickname":
+        console.log(parsed.payload);
+        socket["nickname"] = parsed.payload;
+        break;
+    }
   });
+  console.log(sockets);
 });
 
 // 이제 http ,ws 서버는 다 같은 port를 공유하게된다.
