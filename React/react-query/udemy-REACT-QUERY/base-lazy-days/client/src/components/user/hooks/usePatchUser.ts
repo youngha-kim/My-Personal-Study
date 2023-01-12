d95b0c09f79be5ea92f2feb8ac1647df5b3c2b11
younha-kim <rladudtmd727@gmail.com>
2023-01-12 23:47:06 +0900

    learn react-qeury mutation

--- React/react-query/udemy-REACT-QUERY/base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ React/react-query/udemy-REACT-QUERY/base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,28 +1,28 @@
 import jsonpatch from 'fast-json-patch';
-
+import { useMutation,UseMutateFunction } from 'react-query';
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
 import { useUser } from './useUser';
 
 // for when we need a server function
-// async function patchUserOnServer(
-//   newData: User | null,
-//   originalData: User | null,
-//  ): Promise<User | null> {
-//   if (!newData || !originalData) return null;
-//   // create a patch for the difference between newData and originalData
-//   const patch = jsonpatch.compare(originalData, newData);
+async function patchUserOnServer(
+  newData: User | null,
+  originalData: User | null,
+ ): Promise<User | null> {
+  if (!newData || !originalData) return null;
+  // create a patch for the difference between newData and originalData
+  const patch = jsonpatch.compare(originalData, newData);
 
-//   // send patched data to the server
-//   const { data } = await axiosInstance.patch(
-//     `/user/${originalData.id}`,
-//     { patch },
-//     {
-//       headers: getJWTHeader(originalData),
-//     },
-//   );
-//   return data.user;
-//  }
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
+    `/user/${originalData.id}`,
+    { patch },
+    {
+      headers: getJWTHeader(originalData),
+    },
+  );
+  return data.user;
+ }
 
 // TODO: update type to UseMutateFunction type
 export function usePatchUser(): (newData: User | null) => void {
