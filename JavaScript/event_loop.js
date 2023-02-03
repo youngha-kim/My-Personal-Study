5de4f29bc5c7e004ef5d1d48f0b058756bcd685e
younha-kim <rladudtmd727@gmail.com>
2023-02-03 22:20:10 +0900

    learn browser event

--- JavaScript/event_loop.js
+++ JavaScript/event_loop.js
@@ -1,7 +1,44 @@
-console.log("HI")
+// let i = 0;
 
-setTimeout( function cb(){
-  console.log('there');
-}, 5000);
+// let start = Date.now();
 
-console.log('JSconfEu')
+// function count() {
+
+//   // 무거운 작업을 쪼갠 후 이를 수행 (*)
+//   do {
+//     i++;
+//   } while (i % 1e6 != 0);
+
+//   if (i == 1e9) {
+//     console.log("처리에 걸린 시간: " + (Date.now() - start) + "ms");
+//   } else {
+//     setTimeout(count); // 새로운 호출을 스케줄링 (**)
+//   }
+
+// }
+
+// count();
+
+
+let i2 = 0;
+
+let start2= Date.now();
+
+function count2() {
+
+  // 스케줄링 코드를 함수 앞부분으로 옮김
+  if (i2 < 1e9 - 1e6) {
+    setTimeout(count2); // 새로운 호출을 스케줄링함
+  }
+
+  do {
+    i2++;
+  } while (i2 % 1e6 != 0);
+
+  if (i2 == 1e9) {
+    console.log("처리에 걸린 시간2: " + (Date.now() - start2) + "ms");
+  }
+
+}
+
+count2();
