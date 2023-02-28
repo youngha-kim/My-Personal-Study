63049a20f04c43eeb4db913d2007636f789167c1
younha-kim <rladudtmd727@gmail.com>
2023-02-28 19:43:17 +0900

    learn next.js

---
+++ Next/nextjs-course-without-ts/pages/index.js
@@ -0,0 +1,7 @@
+ // our-domain.com/
+
+ function Hompage(){
+  return <h1>The Home Page</h1>
+ }
+
+ export default Hompage;
---
+++ Next/nextjs-course-without-ts/pages/news.js
@@ -0,0 +1,7 @@
+ // our-domain.com/
+
+ function Hompage(){
+  return <h1>The News Page</h1>
+ }
+
+ export default Hompage;
